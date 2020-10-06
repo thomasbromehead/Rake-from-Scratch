0862f01782ba91c5258fa2cc478d2fa442590e73
Thomas Bromehead <t_bromehead@yahoo.fr>
2020-10-06 18:39:30 +0000

    Yield dependencies if not empty array

--- Rakefile
+++ Rakefile
@@ -1,6 +1,13 @@
-puts "I've been loaded"
-
-task :hello do
+task :hello do 
+  # puts "I am being run when .call is invoked!"
   puts "hello"
+end
+
+task :world do
+  puts "world"
+end
+
+task :hello_world, [:hello, :world] do |dependencies|
+   puts "Called the following tasks as dependencies: #{dependencies.join(', ')}"
 end
 
--- main.rb
+++ main.rb
@@ -1,29 +1,43 @@
 TASKS = {}
 
 class Task
-  def initialize(name, action)
+  attr_reader :dependencies, :action
+  def initialize(name, dependencies, action)
     @name = name
     @action = action
+    @dependencies = dependencies
   end
 
   def call
     # Instance variables are nil by default
     return if @already_run
-    action.call
+    p action
+    @action.call
     @already_run = true
   end
 end
 
 # The task method will be called when loading the Rakefile so needs to be defined
-def task(name, &block)
-  p name
-  p block
-  command = Task.new(name, block)
+def task(name, dependencies = [], &block)
+  yield dependencies if !dependencies.empty?
+  command = Task.new(name, dependencies, block)
   TASKS[name.to_s] = command
 end
 
+# Load doesn't require a .rb extension and will run the code
 load "./Rakefile"
 
 ARGV.each do |command|
-  TASKS[command].call
+  if TASKS.has_key?(command)
+    deps = TASKS[command].dependencies
+    if !deps.empty?
+      deps.each do |dep|
+        TASKS[dep.to_s].action.call
+      end
+    end
+    TASKS[command].call
+  else
+    puts "Sorry there is no task defined with name #{command.name}"
+  end
 end
+
